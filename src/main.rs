ea63a32bc1d15eb9e89751c877d921c6a05ad75a
Bart Massey <bart.massey@gmail.com>
2026-02-12 17:39:50 -0800

    got things smoothly working

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,54 @@ use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
 
 use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
 use microbit::{
     board::Board,
     hal::{
-        prelude::*,
         Timer,
         gpio::Level,
     },
 };
+
+/// Number of refresh cycles to stay on a given brightness.
+const CYCLES_PER_BRIGHTNESS: usize = 4;
+/// Microseconds of duty per brightness step.
+const US_PER_BRIGHTNESS: u32 = 4;
+/// Number of brightness steps.
+const BRIGHTNESSES: u32 = 256;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
-    let pin_r = board.pins.p0_10.into_push_pull_output(Level::Low);
-    let pin_g = board.pins.p0_09.into_push_pull_output(Level::Low);
-    let pin_b = board.pins.p1_02.into_push_pull_output(Level::Low);
+    let pin_r = board.edge.e08.into_push_pull_output(Level::Low);
+    let pin_g = board.edge.e09.into_push_pull_output(Level::Low);
+    let pin_b = board.edge.e16.into_push_pull_output(Level::Low);
     let mut pins = [pin_r.degrade(), pin_g.degrade(), pin_b.degrade()];
     let mut index = 0;
-    let states = [4, 2, 1, 6, 3, 5, 7, 0];
+    let states = [0b100, 0b010, 0b001, 0b011, 0b101, 0b110, 0b111, 0];
     let nstates = states.len();
     
     loop {
         let state = states[index];
-        for (i, p) in pins.iter_mut().enumerate() {
-            if (state >> i) & 1 == 1 {
-                p.set_high().unwrap();
-            } else {
-                p.set_low().unwrap();
+        for brightness in (1..BRIGHTNESSES).chain((1..BRIGHTNESSES).rev()) {
+            for _ in 0..CYCLES_PER_BRIGHTNESS {
+                for (i, p) in pins.iter_mut().enumerate() {
+                    if (state >> i) & 1 == 1 {
+                        p.set_low().unwrap();
+                    } else {
+                        p.set_high().unwrap();
+                    }
+                }
+                timer.delay_us(US_PER_BRIGHTNESS * brightness);
+
+                for p in pins.iter_mut() {
+                        p.set_high().unwrap();
+                }
+                timer.delay_us(US_PER_BRIGHTNESS * (BRIGHTNESSES - brightness));
             }
         }
-        timer.delay_ms(500u16);
         index = (index + 1) % nstates;
     }
 }
