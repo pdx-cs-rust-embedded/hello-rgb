3e9cc21c9c36023591f3ac0fed4ace3d652cab8d
Bart Massey <bart.massey@gmail.com>
2026-02-16 00:56:31 -0800

    cleaned up pin stuff

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # hello-rgb: MB2 Rust external RGB demo
 Bart Massey 2023
 
-This MicroBit v2 program cycles an external RGB connected to
-GPIO pins. It cycles through eight states in the order
+This MB2 program cycles an external RGB connected to GPIO
+pins. It cycles through eight states in the order
 
     R, G, B, Y, C, M, W, -
 
@@ -10,17 +10,15 @@ at 0.5s per step.
 
 The wires of the LED should be connected to the MB2
 according to the following table. LED is the LED wire to
-connect to. R is a current limit resistor value in ohms. MB2
-is the pin on the MB2 edge connector to connect the other
-end of the resistor to. The cathode of the LED should be
-connect to ground. mA is the current used by that component
-in milliamps.
+connect to. MB2 is the pin on the MB2 edge connector to
+connect the other end of the resistor to. The anode (long
+lead) of the LED should be connect to power.
 
-    LED        R    MB2  mA
+    LED        MB2
     
-    Red        120  P08  11.0
-    Green      390  P09  2.32
-    Blue       120  P16  5.36
+    Red        P08
+    Green      P09
+    Blue       P16
 
 The resistor values were chosen to give roughly equal
 brightness to all component colors.
--- src/main.rs
+++ src/main.rs
@@ -16,24 +16,24 @@ fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
-    let pin_r = board.edge.e08.into_push_pull_output(Level::Low);
-    let pin_g = board.edge.e09.into_push_pull_output(Level::Low);
-    let pin_b = board.edge.e16.into_push_pull_output(Level::Low);
+    let pin_r = board.edge.e08.into_push_pull_output(Level::High);
+    let pin_g = board.edge.e09.into_push_pull_output(Level::High);
+    let pin_b = board.edge.e16.into_push_pull_output(Level::High);
     let mut pins = [pin_r.degrade(), pin_g.degrade(), pin_b.degrade()];
     let mut index = 0;
-    let states = [4, 2, 1, 6, 3, 5, 7, 0];
+    let states = [0b100, 0b010, 0b001, 0b011, 0b101, 0b110, 0b111, 0];
     let nstates = states.len();
 
     loop {
         let state = states[index];
         for (i, p) in pins.iter_mut().enumerate() {
-            if (state >> i) & 1 == 1 {
-                p.set_high().unwrap();
-            } else {
+            if (state >> (2 - i)) & 1 == 1 {
                 p.set_low().unwrap();
+            } else {
+                p.set_high().unwrap();
             }
         }
-        timer.delay_ms(500);
+        timer.delay_ms(1000);
         index = (index + 1) % nstates;
     }
 }
